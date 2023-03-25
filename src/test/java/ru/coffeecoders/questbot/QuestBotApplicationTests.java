8329285653453cf4d554df5dec4972fc8d70c732
ezuykow <zuy1996@gmail.com>
2023-03-25 15:06:58 +0300

    Initial commit

---
+++ src/test/java/ru/coffeecoders/questbot/QuestBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.coffeecoders.questbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
