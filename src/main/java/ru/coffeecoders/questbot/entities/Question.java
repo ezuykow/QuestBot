de03cc47c28d83758b75178c72e55c7b4639fbe4
ezuykow <zuy1996@gmail.com>
2023-03-27 18:32:06 +0300

    Добавил сущность Question;

---
+++ src/main/java/ru/coffeecoders/questbot/entities/Question.java
@@ -0,0 +1,108 @@
+/**
+ * Author: ezuykow
+ */
+
+package ru.coffeecoders.questbot.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "questions")
+public class Question {
+
+    @Id
+    @Column(name = "question_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long questionId;
+
+    @Column(name = "question")
+    private String question;
+
+    @Column(name = "answer_format")
+    private String answerFormat;
+
+    @Column(name = "answer")
+    private String answer;
+
+    @Column(name = "map_url")
+    private String mapUrl;
+
+    @Column(name = "last_usage")
+    private Date lastUsage;
+
+    public long getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(long questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswerFormat() {
+        return answerFormat;
+    }
+
+    public void setAnswerFormat(String answerFormat) {
+        this.answerFormat = answerFormat;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getMapUrl() {
+        return mapUrl;
+    }
+
+    public void setMapUrl(String mapUrl) {
+        this.mapUrl = mapUrl;
+    }
+
+    public Date getLastUsage() {
+        return lastUsage;
+    }
+
+    public void setLastUsage(Date lastUsage) {
+        this.lastUsage = lastUsage;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Question question = (Question) o;
+        return questionId == question.questionId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(questionId);
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "questionId=" + questionId +
+                ", question='" + question + '\'' +
+                ", answerFormat='" + answerFormat + '\'' +
+                ", answer='" + answer + '\'' +
+                ", mapUrl present='" + !mapUrl.isBlank() + '\'' +
+                ", lastUsage=" + lastUsage +
+                '}';
+    }
+}
