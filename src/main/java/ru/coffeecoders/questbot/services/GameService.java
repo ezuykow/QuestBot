d3ec1db154f6de7a6b46db12cd64fd97e23ab92f
Anatoliy <t7313@ya.ru>
2023-03-29 18:48:19 +0400

    Обновил сервисы(без методов)

--- src/main/java/ru/coffeecoders/questbot/services/GameService.java
+++ src/main/java/ru/coffeecoders/questbot/services/GameService.java
@@ -1,7 +1,13 @@
 package ru.coffeecoders.questbot.services;
 
 import org.springframework.stereotype.Service;
+import ru.coffeecoders.questbot.repositories.GameRepository;
 
 @Service
 public class GameService {
+    private final GameRepository gameRepository;
+
+    public GameService(GameRepository gameRepository) {
+        this.gameRepository = gameRepository;
+    }
 }
--- src/main/java/ru/coffeecoders/questbot/services/PlayerService.java
+++
@@ -1,7 +0,0 @@
-package ru.coffeecoders.questbot.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PlayerService {
-}
