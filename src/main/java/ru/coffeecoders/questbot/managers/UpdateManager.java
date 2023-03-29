92b070ee9d1dfa8e0f8c55df77c01b2a56db8235
Anatoliy <t7313@ya.ru>
2023-03-29 10:11:09 +0400

    Добавил listner и updateManager

---
+++ src/main/java/ru/coffeecoders/questbot/managers/UpdateManager.java
@@ -0,0 +1,33 @@
+package ru.coffeecoders.questbot.managers;
+
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class UpdateManager {
+
+    public void performUpdate(Update update) {
+        Optional<String> textOpt = tryGetTextMessage(update.message());
+        textOpt.ifPresent(this::checkTextForCommand);
+
+    }
+
+    private void checkTextForCommand(String text) {
+        if (text.matches("/.*")) {
+            //TODO
+        }
+    }
+
+    private Optional<String> tryGetTextMessage(Message message) {
+        try {
+            return Optional.of(message.text());
+        } catch (NullPointerException e) {
+            return Optional.empty();
+        }
+    }
+
+
+}
