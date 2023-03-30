618a4d366d010e96292fc1a2dd9577d590681185
anna <w0nderfly@live.com>
2023-03-30 15:03:34 +0500

    Один енам с атрибутом вместо трех разных. Убрала разбитие на подстроки. Заменила if-else на switch

--- src/main/java/ru/coffeecoders/questbot/commands/Commands.java
+++ src/main/java/ru/coffeecoders/questbot/commands/Commands.java
@@ -2,45 +2,34 @@ package ru.coffeecoders.questbot.commands;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Component
 public class Commands {
-    public enum AdminsCommands {
-        NEWADMIN,
-        NEWGAME,
-        PLAYING,
-        ADDQUESTION,
-        DELETEQUESTION,
-        EDITQUESTION,
-        SHOWQUESTIONS,
-        S
+    public enum AllCommands {
+        NEWADMIN(Attribute.ADMIN),
+        NEWGAME(Attribute.ADMIN),
+        PLAYING(Attribute.ADMIN),
+        ADDQUESTION(Attribute.ADMIN),
+        DELETEQUESTION(Attribute.ADMIN),
+        EDITQUESTION(Attribute.ADMIN),
+        SHOWQUESTIONS(Attribute.ADMIN),
+        S(Attribute.ADMIN),
+        ADMINON(Attribute.GLOBALADMIN),
+        START(Attribute.PLAYER);
+
+        private final Attribute attribute;
+
+        AllCommands(Attribute attribute) {
+            this.attribute = attribute;
+        }
+
+        public Attribute getAttribute() {
+            return attribute;
+        }
     }
 
-    public enum PlayersCommands {
-        START
-    }
-
-    public enum GlobalAdminsCommands {
-        ADMIN_ON
-    }
-
-    public List<String> getAllAdminsCommands() {
-        return Arrays.stream(Commands.AdminsCommands.values())
-                .map(Commands.AdminsCommands::name)
-                .toList();
-    }
-
-    public List<String> getAllGlobalAdminsCommands() {
-        return Arrays.stream(Commands.GlobalAdminsCommands.values())
-                .map(Commands.GlobalAdminsCommands::name)
-                .toList();
-    }
-
-    public List<String> getAllPlayersCommands() {
-        return Arrays.stream(Commands.PlayersCommands.values())
-                .map(Commands.PlayersCommands::name)
-                .toList();
+    public enum Attribute {
+        GLOBALADMIN,
+        ADMIN,
+        PLAYER
     }
 }
--- src/main/java/ru/coffeecoders/questbot/commands/CommandsManager.java
+++ src/main/java/ru/coffeecoders/questbot/commands/CommandsManager.java
@@ -13,62 +13,51 @@ import org.springframework.stereotype.Component;
 public class CommandsManager {
     private final AdminsCommandsManager adminsCommandsManager;
     private final PlayersCommandsManager playersCommandsManager;
-    private final Commands commands;
     private final AdminService adminService;
     private final AdminChatService adminChatService;
     private final GlobalChatService globalChatService;
     private final CommandsManagerMsgSender managerMsgSender;
 
     public CommandsManager(AdminsCommandsManager adminsCommandsManager, PlayersCommandsManager playersCommandsManager,
-                           Commands commands, AdminService adminService, AdminChatService adminChatService,
+                           AdminService adminService, AdminChatService adminChatService,
                            GlobalChatService globalChatService, CommandsManagerMsgSender managerMsgSender) {
         this.adminsCommandsManager = adminsCommandsManager;
         this.playersCommandsManager = playersCommandsManager;
-        this.commands = commands;
         this.adminService = adminService;
         this.adminChatService = adminChatService;
         this.globalChatService = globalChatService;
         this.managerMsgSender = managerMsgSender;
     }
 
-
     public void manageCommand(Update update) {
-        String msg = update.message().text().trim();
-        int delimiter = msg.indexOf(" ");
-        String command;
-        String other;
-        if (delimiter == -1) {
-            command = msg.substring(1).toUpperCase();
-            other = "";
-        } else {
-            command = msg.substring(1, delimiter).toUpperCase();
-            other = msg.substring(delimiter + 1);
+        String command = update.message().text().trim().substring(1).toUpperCase();
+        try {
+            Commands.Attribute attribute = Commands.AllCommands.valueOf(command).getAttribute();
+            switch (attribute) {
+                case GLOBALADMIN -> checkAndSendGlobalAdminsCommand(update, command);
+                case PLAYER -> checkAndSendPlayersCommand(update, command);
+                case ADMIN -> checkAndSendAdminsCommand(update, command);
+            }
+        } catch (IllegalArgumentException e) {
+            managerMsgSender.sendInvalidCommandMsg(update);
         }
-
-        if (commands.getAllGlobalAdminsCommands().contains(command)) {
-            checkAndSendGlobalAdminsCommand(update, command, other);
-        } else if (commands.getAllAdminsCommands().contains(command)) {
-            checkAndSendAdminsCommand(update, command, other);
-        } else if (commands.getAllPlayersCommands().contains(command)) {
-            checkAndSendPlayersCommand(update, command, other);
-        } else managerMsgSender.sendInvalidCommandMsg(update);
     }
 
-    private void checkAndSendPlayersCommand(Update update, String command, String other) {
+    private void checkAndSendPlayersCommand(Update update, String command) {
         if (globalChatService.findById(update.message().chat().id()).isPresent) {
-            playersCommandsManager.doSome(update, command, other);
+            playersCommandsManager.doSome(update, command);
         } else managerMsgSender.sendNotGlobalChatMsg(update);
     }
 
-    private void checkAndSendAdminsCommand(Update update, String command, String other) {
+    private void checkAndSendAdminsCommand(Update update, String command) {
         if (adminChatService.findById(update.message().chat().id()).isPresent) {
-            checkAndSendGlobalAdminsCommand(update, command, other);
+            checkAndSendGlobalAdminsCommand(update, command);
         } else managerMsgSender.sendNotAdminChatMsg(update);
     }
 
-    private void checkAndSendGlobalAdminsCommand(Update update, String command, String other) {
+    private void checkAndSendGlobalAdminsCommand(Update update, String command) {
         if (adminService.findById(update.message().from().id()).isPresent) {
-            adminsCommandsManager.doSome(update, command, other);
+            adminsCommandsManager.doSome(update, command);
         } else managerMsgSender.sendNotAdminMsg(update);
     }
 }
