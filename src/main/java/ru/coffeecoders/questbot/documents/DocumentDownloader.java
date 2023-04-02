d2130fb32c50edeadcb6686ec190afb8fd073bf6
ezuykow <zuy1996@gmail.com>
2023-04-02 13:47:40 +0300

    Сделал DocumentDownloader

--- src/main/java/ru/coffeecoders/questbot/documents/DocumentDownloader.java
+++ src/main/java/ru/coffeecoders/questbot/documents/DocumentDownloader.java
@@ -1,13 +1,17 @@
 package ru.coffeecoders.questbot.documents;
 
 import com.pengrad.telegrambot.model.Document;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
 
 /**
  * @author ezuykow
@@ -17,26 +21,61 @@ public class DocumentDownloader {
 
     Logger logger = LoggerFactory.getLogger(DocumentDownloader.class);
 
+    @Value("${telegram.bot.api.prefix}")
+    private String botApiUrlPrefix;
+    @Value("${telegram.bot.token}")
+    private String botToken;
+    @Value("${telegram.bot.api.method.getFile}")
+    private String apiGetFileMethod;
+    @Value("${telegram.file.api.prefix}")
+    private String fileApiPrefix;
+    @Value("${document.temp.file.prefix}")
+    private String tempFileNamePrefix;
+    @Value("${document.temp.file.suffix}")
+    private String tempFileNameSuffix;
+
     /**
      * Принимает документ из {@link com.pengrad.telegrambot.model.Update},
-     * создает временный файл в файловой системе (удаляет его при остановке JVM),
+     * создает временный файл в файловой системе (не забудьте его удалить),
      * в который пытается скачать документ с серверов Telegram
-     * @param document документ из апдейта. Не может быть {@code NULL}
+     * @param doc документ из апдейта. Не может быть {@code NULL}
      * @return временный файл, эквивалентный файлу на серверах Telegram
      */
-    public File downloadDocument(Document document) {
+    public File downloadDocument(Document doc) {
         try {
-            return null; //TODO !!!
-        } /*catch (IOException e) {
-            logger.error("Не получилось стянуть файл \uD83E\uDD74");
+            return downloadFile(fileUrl(doc), newTempFile());
+        } catch (IOException e) {
+            logger.error("Ошибка загрузки файла \uD83E\uDD74");
             throw new RuntimeException(e);
-        }*/
-        finally { //TODO remove!
-            return null;
         }
     }
 
-    private File downloadFile(/*URL fileURL, File tempFile*/) {
-        return null; //TODO !!!
+    private File downloadFile(URL fileURL, File tempFile) throws IOException {
+        try (ReadableByteChannel rbc = Channels.newChannel(fileURL.openStream());
+             FileOutputStream fos = new FileOutputStream(tempFile)) {
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            return tempFile;
+        }
+    }
+
+    private URL fileUrl(Document doc) throws IOException {
+        return new URL(fileApiPrefix + botToken + "/" + getFilePath(doc));
+    }
+
+    private String getFilePath(Document doc) throws IOException {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(fileInfoUrl(doc).openStream()))) {
+            return new JSONObject(in.readLine()).getJSONObject("result").getString("file_path");
+        } catch (JSONException e) {
+            logger.error("Вернулся кривой JSON при запросе информации о документе");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private URL fileInfoUrl(Document doc) throws IOException {
+        return new URL(botApiUrlPrefix + botToken + apiGetFileMethod + doc.fileId());
+    }
+
+    private File newTempFile() throws IOException {
+        return File.createTempFile(tempFileNamePrefix, tempFileNameSuffix);
     }
 }
