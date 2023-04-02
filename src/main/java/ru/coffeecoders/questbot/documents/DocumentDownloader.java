8b2939b6c4adf4e641bb19c43b7de2d2c69d02ff
ezuykow <zuy1996@gmail.com>
2023-04-02 12:26:31 +0300

    работаю над Downloader

--- src/main/java/ru/coffeecoders/questbot/documents/DocumentDownloader.java
+++ src/main/java/ru/coffeecoders/questbot/documents/DocumentDownloader.java
@@ -1,8 +1,13 @@
 package ru.coffeecoders.questbot.documents;
 
 import com.pengrad.telegrambot.model.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * @author ezuykow
@@ -10,7 +15,28 @@ import java.io.File;
 @Component
 public class DocumentDownloader {
 
+    Logger logger = LoggerFactory.getLogger(DocumentDownloader.class);
+
+    /**
+     * Принимает документ из {@link com.pengrad.telegrambot.model.Update},
+     * создает временный файл в файловой системе (удаляет его при остановке JVM),
+     * в который пытается скачать документ с серверов Telegram
+     * @param document документ из апдейта. Не может быть {@code NULL}
+     * @return временный файл, эквивалентный файлу на серверах Telegram
+     */
     public File downloadDocument(Document document) {
-        return null;
+        try {
+            return null; //TODO !!!
+        } /*catch (IOException e) {
+            logger.error("Не получилось стянуть файл \uD83E\uDD74");
+            throw new RuntimeException(e);
+        }*/
+        finally { //TODO remove!
+            return null;
+        }
+    }
+
+    private File downloadFile(/*URL fileURL, File tempFile*/) {
+        return null; //TODO !!!
     }
 }
