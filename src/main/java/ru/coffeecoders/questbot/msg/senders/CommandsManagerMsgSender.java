4d14584dc627a27a3cd3542537808c71e657a486
ezuykow <zuy1996@gmail.com>
2023-04-11 08:21:32 +0300

    Добавил @Component в CommandsManagerMsgSender

--- src/main/java/ru/coffeecoders/questbot/msg/senders/CommandsManagerMsgSender.java
+++ src/main/java/ru/coffeecoders/questbot/msg/senders/CommandsManagerMsgSender.java
@@ -6,7 +6,9 @@ import com.pengrad.telegrambot.response.SendResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommandsManagerMsgSender {
 
     private final Logger logger = LoggerFactory.getLogger(CommandsManagerMsgSender.class);
