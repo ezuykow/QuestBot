0a250a14b59af3b78d68d8883030854ac3927cfd
Dmabl <sdfghj@dg.jnb>
2023-04-12 12:14:58 +0200

    PagingAndSortingRepository<Question, Long> - и код сильно упростится

--- src/main/java/ru/coffeecoders/questbot/keyboards/admins/creators/EditDeleteKeyboardCreator.java
+++ src/main/java/ru/coffeecoders/questbot/keyboards/admins/creators/EditDeleteKeyboardCreator.java
@@ -11,13 +11,13 @@ public class EditDeleteKeyboardCreator {
 
 
     @Value("${keyboard.allQuestions.edit}")
-    private static String edits;
+    private String edits;
 
     @Value("${keyboard.allQuestions.delete}")
-    private static String deletes;
+    private  String deletes;
 
 
-    private EditDeleteKeyboardCreator(EditDeleteKeyboardCreator editDeleteKeyboardCreator) {
+    private EditDeleteKeyboardCreator() {
         this.editDeleteKeyboardCreator = editDeleteKeyboardCreator;
     }
 
@@ -30,13 +30,10 @@ public class EditDeleteKeyboardCreator {
 
 
     public static Keyboard createQuestionKeyboard() {
-
-        
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup(
-                makeKeyboard
-                        (makeRows
-                                (makeButtonArray()))
-        );
+        EditDeleteKeyboardCreator keyboardCreator = new EditDeleteKeyboardCreator();
+        KeyboardButton[] buttonArray = keyboardCreator.makeButtonArray();
+        KeyboardButton[][] buttonRows = keyboardCreator.makeRows(buttonArray);
+        ReplyKeyboardMarkup keyboardMarkup = keyboardCreator.makeKeyboard(buttonRows);
         return keyboardMarkup;
     }
 
@@ -56,15 +53,3 @@ public class EditDeleteKeyboardCreator {
         return new ReplyKeyboardMarkup(buttonRows);
     }
 }
-// добавляем сразу в базу данных
-//    public static void  addQuestions() {
-//    // возможно парсить по блокам через инлайн "вопрос" - "ответ" - "группа" - "показать"? введите вопрос /вопрос: Кто обитает на дне океана введите ответ /ответ: Спанч боб
-//        return null;
-//    }
-//    public static void deleteQuestions() {
-//    }
-//
-//    public static void editQuestions() {
-//        return ;
-//    }
-//}
