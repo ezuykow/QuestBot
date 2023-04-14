59436debc0f0a81c6c3586d8f61bd16fa2005360
anna <w0nderfly@live.com>
2023-04-14 11:02:43 +0500

    Изменила метод showTasks

--- src/main/java/ru/coffeecoders/questbot/commands/actions/PlayersCommandsActions.java
+++ src/main/java/ru/coffeecoders/questbot/commands/actions/PlayersCommandsActions.java
@@ -2,12 +2,15 @@ package ru.coffeecoders.questbot.commands.actions;
 
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+import ru.coffeecoders.questbot.entities.Task;
 import ru.coffeecoders.questbot.entities.Team;
 import ru.coffeecoders.questbot.msg.senders.MessageSender;
 import ru.coffeecoders.questbot.services.GameService;
+import ru.coffeecoders.questbot.services.QuestionService;
 import ru.coffeecoders.questbot.services.TaskService;
 import ru.coffeecoders.questbot.services.TeamService;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +24,7 @@ public class PlayersCommandsActions {
     private final TaskService taskService;
     private final GameService gameService;
     private final MessageSender msgSender;
+    private final QuestionService questionService;
     private final KeyboardFactory keyboardFactory;
     private final Environment env;
 
@@ -28,12 +32,14 @@ public class PlayersCommandsActions {
                                   TaskService taskService,
                                   GameService gameService,
                                   MessageSender msgSender,
+                                  QuestionService questionService,
                                   KeyboardFactory keyboardFactory,
                                   Environment env) {
         this.teamService = teamService;
         this.taskService = taskService;
         this.gameService = gameService;
         this.msgSender = msgSender;
+        this.questionService = questionService;
         this.keyboardFactory = keyboardFactory;
         this.env = env;
     }
@@ -59,8 +65,7 @@ public class PlayersCommandsActions {
     public void showTasks(long chatId) {
         //TODO добавить проверку, есть ли запущенная игра, чтоб не проверять больше ничего
         try {
-            List<String> tasks = taskService.findByGameName(gameService.findAll().get(0).getGameName());
-            msgSender.send(chatId, createTextFromList(tasks));
+            msgSender.send(chatId, createTextFromList(getAllTasksAsString()));
         } catch (IndexOutOfBoundsException exception) {
             msgSender.send(chatId, env.getProperty("messages.players.noCurrentGame"));
         }
@@ -91,23 +96,19 @@ public class PlayersCommandsActions {
         }
     }
 
-    /**
-     * Получает данные в Мар, отдает в виде строки
-     *
-     * @param map пары название команды-счет
-     * @return строка, содержащая счета всех команд
-     */
+    private List<String> getAllTasksAsString() throws IndexOutOfBoundsException{
+        List<Integer> tasksIds = taskService.findByGameName(gameService.findAll().get(0).getGameName())
+                .stream().map(Task::getQuestionId);
+        List<String> tasks = new ArrayList<>();
+        tasksIds.forEach(a -> tasks.add(questionService.findById(a).get().getQuestion()));
+        return tasks;
+    }
     private String createTextFromMap(Map<String, Integer> map) {
         StringBuilder sb = new StringBuilder();
         map.forEach((name, score)-> sb.append(name).append(": ").append(score).append('\n'));
         return sb.toString();
     }
-    /**
-     * Получает данные в List, отдает в виде строки
-     *
-     * @param list лист строк
-     * @return строка, содержащая все вопросы
-     */
+
     private String createTextFromList(List<String> list) {
         StringBuilder sb = new StringBuilder();
         list.forEach(a -> sb.append("* ").append(a).append("\n"));
