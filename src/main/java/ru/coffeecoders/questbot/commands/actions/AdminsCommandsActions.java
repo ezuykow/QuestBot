0716fa49e172cf4d63c4872ac2d437ed7fb55100
ezuykow <zuy1996@gmail.com>
2023-04-17 11:30:56 +0300

    add GlobalChat saving after /start cmd

--- src/main/java/ru/coffeecoders/questbot/commands/actions/AdminsCommandsActions.java
+++ src/main/java/ru/coffeecoders/questbot/commands/actions/AdminsCommandsActions.java
@@ -3,9 +3,11 @@ package ru.coffeecoders.questbot.commands.actions;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import ru.coffeecoders.questbot.entities.AdminChat;
+import ru.coffeecoders.questbot.entities.GlobalChat;
 import ru.coffeecoders.questbot.models.ExtendedUpdate;
 import ru.coffeecoders.questbot.senders.MessageSender;
 import ru.coffeecoders.questbot.services.AdminChatService;
+import ru.coffeecoders.questbot.services.GlobalChatService;
 import ru.coffeecoders.questbot.viewers.QuestionsViewer;
 
 @Component
@@ -14,16 +16,21 @@ public class AdminsCommandsActions {
     private final MessageSender msgSender;
     private final QuestionsViewer questionsViewer;
     private final AdminChatService adminChatService;
+    private final GlobalChatService globalChatService;
     private final Environment env;
 
-    private AdminsCommandsActions(MessageSender msgSender, QuestionsViewer questionsViewer, AdminChatService adminChatService, Environment env) {
+    private AdminsCommandsActions(MessageSender msgSender, QuestionsViewer questionsViewer, AdminChatService adminChatService, GlobalChatService globalChatService, Environment env) {
         this.msgSender = msgSender;
         this.questionsViewer = questionsViewer;
         this.adminChatService = adminChatService;
+        this.globalChatService = globalChatService;
         this.env = env;
     }
 
     public void performStartCmd(ExtendedUpdate update) {
+        GlobalChat globalChat = new GlobalChat();
+        globalChat.setTgChatId(update.getMessageChatId());
+        globalChatService.save(globalChat);
         msgSender.send(update.getMessageChatId(), env.getProperty("messages.welcome"));
     }
 
