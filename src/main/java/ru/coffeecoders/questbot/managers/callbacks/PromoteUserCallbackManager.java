4143a136e9d16fd1bb34179f278595f1274d2ea4
ezuykow <zuy1996@gmail.com>
2023-04-21 16:45:14 +0300

    Refactor

--- src/main/java/ru/coffeecoders/questbot/managers/callbacks/PromoteUserCallbackManager.java
+++ src/main/java/ru/coffeecoders/questbot/managers/callbacks/PromoteUserCallbackManager.java
@@ -71,7 +71,7 @@ public class PromoteUserCallbackManager {
     private void sendPromotionMessage(long chatId, String data) {
         msgSender.send(chatId,
                 data.substring(data.indexOf(".") + 1, data.lastIndexOf("."))
-                        + env.getProperty("messages.admins.userPromoted")
+                        + env.getProperty("messages.owner.userPromoted")
         );
     }
 }
