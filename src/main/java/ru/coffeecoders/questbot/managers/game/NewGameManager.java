5ce2710d22b434827fd31628a3a7664639aabc24
ezuykow <zuy1996@gmail.com>
2023-04-22 09:15:43 +0300

    Создал NewGameManager.java;

---
+++ src/main/java/ru/coffeecoders/questbot/managers/game/NewGameManager.java
@@ -0,0 +1,14 @@
+package ru.coffeecoders.questbot.managers.game;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author ezuykow
+ */
+@Component
+public class NewGameManager {
+
+    public void startCreatingGame() {
+
+    }
+}
