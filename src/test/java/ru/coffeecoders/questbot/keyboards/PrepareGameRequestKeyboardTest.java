58600ca5e7d40434175f9b8588276e5a4134bb6c
anna <w0nderfly@live.com>
2023-04-29 20:41:18 +0500

    Убрала аннотацию

--- src/test/java/ru/coffeecoders/questbot/keyboards/PrepareGameRequestKeyboardTest.java
+++ src/test/java/ru/coffeecoders/questbot/keyboards/PrepareGameRequestKeyboardTest.java
@@ -2,15 +2,11 @@ package ru.coffeecoders.questbot.keyboards;
 
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-@ExtendWith(MockitoExtension.class)
 class PrepareGameRequestKeyboardTest {
-
     @Test
     void createKeyboardTest() {
         long adminId = 1L;
