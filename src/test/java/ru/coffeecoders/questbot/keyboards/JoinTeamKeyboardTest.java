34100864f076e42bbaaeeac1353f3cb490db44d9
anna <w0nderfly@live.com>
2023-04-29 20:40:26 +0500

    Убрала аннотацию

--- src/test/java/ru/coffeecoders/questbot/keyboards/JoinTeamKeyboardTest.java
+++ src/test/java/ru/coffeecoders/questbot/keyboards/JoinTeamKeyboardTest.java
@@ -2,16 +2,12 @@ package ru.coffeecoders.questbot.keyboards;
 
 import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-@ExtendWith(MockitoExtension.class)
-class JoinTeamKeyboardTest {
 
-    @Test
+class JoinTeamKeyboardTest {    @Test
     void createKeyboardTest() {
         String teamName1 = "teamName1";
         String teamName2 = "teamName2";
